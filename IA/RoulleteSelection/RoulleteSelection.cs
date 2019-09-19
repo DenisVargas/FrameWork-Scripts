af11fe74a606775d5fe23a619d5adb2421fe637a
DenisVargas <denis.vargasrivero@outlook.com>
2019-09-18 23:23:55 -0300

    Update RoulleteSelection.cs
    Añadido un NameSpace para la ruleta.
    Cambiado la lista de entrada (Parámetro) por una collección Genérica.

--- IA/RoulleteSelection/RoulleteSelection.cs
+++ IA/RoulleteSelection/RoulleteSelection.cs
@@ -1,28 +1,27 @@
 Using System.Collections.Generic;
 
-public static class RoulleteSelection
+namespace IA.RandomSelections
 {
-	public static int Roullete(List<float> list)
+    public static class RoulleteSelection
     {
-        float sum = 0;
-        foreach (var Numero in list)
-            sum += Numero;
-			
-        List<float> newValues = new List<float>();
-        foreach (var Numero in list)
+        public static int Roullete(IEnumerable<float> list)
         {
-            newValues.Add(Numero / sum);
+            float TotalSum = 0;
+            float RandomIndex = UnityEngine.Random.Range(0f, 1f);
+            float Sum2 = 0;
+            foreach (var Numero in list)
+                TotalSum += Numero;
+                
+            List<float> newValues = new List<float>();
+            foreach (var Numero in list)
+                newValues.Add(Numero / TotalSum);
+
+            for (int i = 0; i < newValues.Count; i++)
+            {
+                Sum2 += newValues[i];
+                if (Sum2 > RandomIndex) return i;
+            }
+            return -1;
         }
-		
-        float RandomIndex = UnityEngine.Random.Range(0f, 1f);
-        
-        float Sum2 = 0;
-        for (int i = 0; i < newValues.Count; i++)
-        {
-            Sum2 += newValues[i];
-            if (Sum2 > RandomIndex)
-                return i;
-        }
-        return -1;
     }
 }
