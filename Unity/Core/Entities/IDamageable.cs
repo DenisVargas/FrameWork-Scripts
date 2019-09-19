9dcf641c01ee5399dfdb4821ee19753e57db20d5
DenisVargas <denis.vargasrivero@outlook.com>
2019-09-18 23:26:29 -0300

    Create IDamageable.cs
    Añadido una Interfaz Core.

---
+++ Unity/Core/Entities/IDamageable.cs
@@ -0,0 +1,22 @@
+﻿/* Determina Comportamiento común para todas los tipos de Unidades. */
+namespace Core.Entities
+{
+    // Todas las unidades que pueden recibir Daño.
+    public interface IDamageable
+    {
+        void GetDamage(params object[] DamageStats);
+    }
+    //Todas las unidades que pueden recibir Daño y tienen 2 estados Básicos: vivos o muertos.
+    public interface IKilleable : IDamageable
+    {
+        bool IsAlive { get; }
+        bool invulnerable { get; }
+    }
+    //Todas las Unidades que pueden tener estadísticas de combate.
+    public interface IAttacker<T>
+    {
+        T GetDamageStats();
+    }
+}
+
+
